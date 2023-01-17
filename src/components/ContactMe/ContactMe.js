3c60d0e35de44cf3394ae95c6672603562738262
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2023-01-17 16:45:01 -0500

    Cards updated and Contact me page added

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,6 +1,18 @@
+import contactCSS from '../ContactMe/ContactMe.module.css'
+import { Grid } from 'semantic-ui-react'
+
 function ContactMe() {
     return(
-        <h1>Contact me</h1>
+        <div className={contactCSS.body}>
+            <h1 className={contactCSS.title}>Contact me</h1>
+            <Grid stackable columns={2}>
+            <Grid.Column>
+                <h4>LinkeIn</h4>
+                <h4>Website</h4>
+                <h4>GitHub</h4>
+            </Grid.Column>
+            </Grid>
+        </div>
     )
 }
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,107 @@
+import projectCSS from '../Projects/Projects.module.css'
+import { Card, Grid, Button, Icon } from 'semantic-ui-react'
+
+
+
 function Projects() {
     return (
-        <h1>Projects</h1>
+        <div className={projectCSS.body}>
+            <h2>Projects</h2>
+            <br/>
+            <br/>
+            <div className={projectCSS.projContainer}>
+                <Grid stackable columns={2}>
+                    <Grid.Column>
+                    <Card className={projectCSS.projCard} style={{ background:'rgb(40, 40, 40)', width:'100%'}}>
+                    <div className={projectCSS.video}>
+                        <iframe width="650" height="360" src="https://www.youtube.com/embed/mhBB_LN-tdE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                    </div>
+                        <Card.Content>
+                            <Card.Header style={{ color: 'white'}}>Prov31</Card.Header>
+                            <Card.Meta style={{ color: 'white'}}>HTML, CSS, JavaScript</Card.Meta>
+                            <Card.Description style={{ color: 'white'}}>
+                                Reading application that sorts one chapter of Proverbs according to the actual date<br/><br/>
+                                ● Created a single-page web application frame utilizing HTML<br/>
+                                ● Utilized CSS to style the application<br/>
+                                ● Utilized JavaScript to interact with DOM and fetch a public API
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <Button>Link</Button>
+                            <Button><Icon name='github'/>GitHub</Button>
+                        </Card.Content>
+                    </Card>
+                    </Grid.Column>
+
+                    <Grid.Column>
+                    <Card className={projectCSS.projCard} style={{ background:'rgb(40, 40, 40)', width:'100%' }}>
+                    <div className={projectCSS.video}>
+                        <iframe width="650" height="360" src="https://www.youtube.com/embed/9g5E654fXz4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                    </div>    
+                        <Card.Content>
+                            <Card.Header style={{ color: 'white'}}>TechPrep</Card.Header>
+                            <Card.Meta style={{ color: 'white'}}>React JS, Bootstrap, JSON database</Card.Meta>
+                            <Card.Description style={{ color: 'white'}}>
+                                Technical interview flashcards app that allows you to create, show/hide answers and delete flashcards<br/><br/>
+                                ● Created a responsive single page web application utilizing React JS<br/>
+                                ● Upgraded the style utilizing Bootstrap<br/>
+                                ● Created a JSON database to store the flashcards and interact with CRUD actions
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <Button>Link</Button>
+                            <Button><Icon name='github'/>GitHub</Button>
+                        </Card.Content>
+                    </Card>
+                    </Grid.Column>
+
+                    <Grid.Column>
+                    <Card className={projectCSS.projCard} style={{ background:'rgb(40, 40, 40)', width:'100%'}}>
+                    <div className={projectCSS.video}>
+                        <iframe width="650" height="360" src="https://www.youtube.com/embed/ZybpPey2g2U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>    
+                        <Card.Content>
+                            <Card.Header style={{ color: 'white'}}>BGrateful</Card.Header>
+                            <Card.Meta style={{ color: 'white'}}>React JS, Semantic UI, Ruby</Card.Meta>
+                            <Card.Description style={{ color: 'white'}}>
+                                Gratitude Journal API<br/><br/>
+                                ● Created a Ruby-based API<br/>
+                                ● Utilized Active Record + Sinatra to perform CRUD actions<br/>
+                                ● Created a frontend React JS<br/>
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <Button>Link</Button>
+                            <Button><Icon name='github'/>GitHub</Button>
+                        </Card.Content>
+                    </Card>
+                    </Grid.Column>
+
+                    <Grid.Column>
+                    <Card className={projectCSS.projCard} style={{ background:'rgb(40, 40, 40)', width:'100%'}}>
+                    <div className={projectCSS.video}>
+                        <iframe width="650" height="360" src="https://www.youtube.com/embed/LWusZ8n1nNE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>    
+                        <Card.Content>
+                            <Card.Header style={{ color: 'white'}}>improvE</Card.Header>
+                            <Card.Meta style={{ color: 'white'}}>React JS, Ruby on Rails, AWS S3, Heroku</Card.Meta>
+                            <Card.Description style={{ color: 'white'}}>
+                                Monorepo Training Platform<br/><br/>
+                                ● User and Admin login implemented on backend<br/>
+                                ● Upload files to Amazon AWS S3 bucket<br/>
+                                ● Heroku deployment<br/>
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <Button>Link</Button>
+                            <Button><Icon name='github'/>GitHub</Button>
+                        </Card.Content>
+                    </Card>
+                    </Grid.Column>
+                </Grid>
+
+            </div>
+        </div>
     )
 }
 
